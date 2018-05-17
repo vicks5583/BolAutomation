0e55ab9cfe681f9ba13ca1e7cfa117f4168cdcf1
ahmed.siddiqui <ahmed.siddiqui@myboltv.net>
2018-05-17 18:51:31 +0500

    bolwala flow enhanced

--- src/test/java/com/automation/Desktop/TestCases/BolwalaClass.java
+++ src/test/java/com/automation/Desktop/TestCases/BolwalaClass.java
@@ -38,38 +38,84 @@ public class BolwalaClass {
 	  }
 	  
 	  @Test
-	  public void Test05_Verify_Bolwala_Login_Link() {
+	  public void Test05_Verify_Location_Dropdown() {
+			  dsl.bolwalaclass.CheckLocationDropdown();
+	  }
+	  
+	  @Test
+	  public void Test06_Verify_Help_Dropdown() {
+			  dsl.bolwalaclass.CheckHelpDropdown();
+	  }
+	  
+	  @Test
+	  public void Test07_Verify_Phone_Number() {
+			  dsl.bolwalaclass.CheckPhoneNumber();
+	  }
+	  
+	  @Test
+	  public void Test08_Verify_Email_Address() {
+			  dsl.bolwalaclass.CheckEmailAddress();
+	  }
+	  
+	  @Test
+	  public void Test09_Verify_Bolwala_Login_Link() {
 			  dsl.bolwalaclass.CheckLinkForLogin();
 	  }
 	  
 	  @Test
-	  public void Test06_Verify_Bolwala_Signup_Link() {
+	  public void Test10_Verify_Bolwala_Signup_Link() {
 			  dsl.bolwalaclass.CheckLinkForSignup();
 	  }
 	  
 	  @Test
-	  public void Test07_Verify_Bolwala_Search_Field() {
+	  public void Test11_Verify_Bolwala_Search_Field() {
 			  dsl.bolwalaclass.CheckSearchField();
 	  }
 	  
 	  @Test
-	  public void Test08_Verify_Top_Banner() {
+	  public void Test12_Verify_Top_Banner() {
 			  dsl.bolwalaclass.CheckTopBanner();
 	  }
 	  
 	  @Test
-	  public void Test09_Verify_Bolwala_Product_Tabs() {
+	  public void Test13_Verify_Bolwala_Product_Tabs() {
 			  dsl.bolwalaclass.CheckProductTabs();
 	  }
 	  
 	  @Test
-	  public void Test10_Verify_First_Product_Presence() {
+	  public void Test14_Verify_First_Product_Presence() {
 			  dsl.bolwalaclass.CheckFirstProduct();
 	  }
 	  
 	  @Test
-	  public void Test11_Search_Verify_Redirect() {
+	  public void Test15_Search_Verify_Redirect() {
 			  dsl.bolwalaclass.Search_Case1();
+	  }
+	  
+	  @Test
+	  public void Test16_Click_CreateAnAccount_To_Redirect(){
+		  dsl.bolwalaclass.ClickLinkForSignup();
+	  }
+	  
+	  @Test
+	  public void Test17_Fill_The_Form(){
+		  dsl.bolwalaclass.FillTheForm();
+	  }
+	  
+	  @Test
+	  public void Test18_Submit_The_Form(){
+		  dsl.bolwalaclass.ClickSignup();
+	  }
+	  
+	  @Test
+	  public void Test19_Redirect_To_My_Account(){
+		  dsl.bolwalaclass.ClickUserName();
+		  dsl.bolwalaclass.ClickMyAccount();
+	  }
+	  
+	  @Test
+	  public void Test20_Verify_User_Pin_Code(){
+		  dsl.bolwalaclass.GetPinNumberAndVerify();
 	  }
 	  
 	  @AfterClass
--- src/test/java/com/automation/Ui/Desktop/BolwalaClass_UI.java
+++ src/test/java/com/automation/Ui/Desktop/BolwalaClass_UI.java
@@ -2,6 +2,8 @@ package com.automation.Ui.Desktop;
 
 import junit.framework.Assert;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,6 +13,8 @@ import com.automation.BaseClass.BaseUi;
 
 public class BolwalaClass_UI extends BaseUi {
 	WebDriver driver;
+	
+	String mobNumber = "";
 
 	public BolwalaClass_UI(WebDriver driver) {
 		super(driver);
@@ -75,6 +79,18 @@ public class BolwalaClass_UI extends BaseUi {
 	@FindBy(linkText = "For Sellers")
 	private WebElement linkForSellers;
 	
+	@FindBy(css = "[class = 'x-location']")
+	private WebElement dropDownLocation;
+	
+	@FindBy(xpath = "//html/body/app-root/app-public/app-header/header/div[2]/div[1]/ul/li[3]/div/div[1]/span")
+	private WebElement dropDownHelp;
+	
+	@FindBy(css = "[class = 'x-phone']")
+	private WebElement phoneNumber;
+	
+	@FindBy(css = "[class = 'x-message']")
+	private WebElement emailAddress;
+	
 	@FindBy(css = "[class = 'login']")
 	private WebElement linkForLogin;
 	
@@ -96,11 +112,47 @@ public class BolwalaClass_UI extends BaseUi {
 	@FindBy(linkText = "Highest")
 	private WebElement tabHighest;
 	
-	@FindBy(xpath = "//html/body/app-root/app-public/div/app-deals/div/div/div/div[2]/div[2]/ul/li[1]")
+	@FindBy(xpath = "//html/body/app-root/app-public/div/app-deals/div/div/div[1]/div[2]/app-discount-card-list/div/ul/li[1]")
 	private WebElement firstProduct;
 	
 	@FindBy(linkText = "A.D Rentals")
 	private WebElement suggestionForRentals;
+	
+	@FindBy(xpath = "//html/body/app-root/app-public/div/app-customer-signup/div/div/div/div/div[1]/div/div[2]/div[1]/input")
+	private WebElement txtFullName;
+	
+	@FindBy(xpath = "//html/body/app-root/app-public/div/app-customer-signup/div/div/div/div/div[1]/div/div[2]/div[2]/input")
+	private WebElement txtMobileNumber;
+	
+	@FindBy(xpath = "//html/body/app-root/app-public/div/app-customer-signup/div/div/div/div/div[1]/div/div[2]/div[3]/input")
+	private WebElement txtEmail;
+	
+	@FindBy(xpath = "/html/body/app-root/app-public/div/app-customer-signup/div/div/div/div/div[1]/div/div[2]/div[4]/input")
+	private WebElement txtPassword;
+	
+	@FindBy(xpath = "//html/body/app-root/app-public/div/app-customer-signup/div/div/div/div/div[1]/div/div[2]/div[5]/input")
+	private WebElement txtConfirmPassword;
+	
+	@FindBy(xpath = "//html/body/app-root/app-public/div/app-customer-signup/div/div/div/div/div[1]/div/div[2]/div[6]/div/div/input")
+	private WebElement chkTermsAndPrivacy;
+	
+	/*@FindBy(xpath = "//html/body/app-root/app-public/div/app-customer-signup/div/div/div/div/div[1]/div/div[2]/div[6]/div/div/span")
+	private WebElement chkTermsAndPrivacy;*/
+	
+	@FindBy(xpath = "//html/body/app-root/app-public/div/app-customer-signup/div/div/div/div/div[1]/div/div[2]/div[7]/a/span")
+	private WebElement btnSignup;
+	
+	@FindBy(css = "[class = 'userName']")
+	private WebElement linkUserName;
+	
+	@FindBy(linkText = "My Account")
+	private WebElement linkMyAccount;
+	
+	@FindBy(css = "[class = 'signout']")
+	private WebElement linkSignout;
+	
+	@FindBy(css = "[class = 'idTag']")
+	private WebElement pinNumber;
 	
 	public void CheckBolwalaTitle() {
 		Assert.assertEquals("Largest Platform of Best Discounts in Pakistan - Bolwala.com", driver.getTitle());
@@ -126,11 +178,26 @@ public class BolwalaClass_UI extends BaseUi {
 		isElementDisplayed(categoryEducation);
 		isElementDisplayed(categoryTransportAndAutomotive);
 		isElementDisplayed(categoryServices);
-		isElementDisplayed(categoryMiscellaneous);
 	}
 	
 	public void CheckLinkForSellers(){
 		isElementDisplayed(linkForSellers);
+	}
+	
+	public void CheckLocationDropdown(){
+		isElementDisplayed(dropDownLocation);
+	}
+	
+	public void CheckHelpDropdown(){
+		isElementDisplayed(dropDownHelp);
+	}
+	
+	public void CheckPhoneNumber(){
+		isElementDisplayed(phoneNumber);
+	}
+	
+	public void CheckEmailAddress(){
+		isElementDisplayed(emailAddress);
 	}
 	
 	public void CheckLinkForLogin(){
@@ -169,4 +236,47 @@ public class BolwalaClass_UI extends BaseUi {
 		bolwalaLogo.click();
 		hardwait(10);
 	}
+	
+	public void ClickLinkForSignup(){
+		isElementDisplayed(linkForSignup);
+		linkForSignup.click();
+		hardwait(5);
+	}
+	
+	public void FillTheForm(){
+		isElementDisplayed(txtFullName);
+		txtFullName.sendKeys("BolAuto");
+		txtMobileNumber.sendKeys("03009"+formatedDate());
+		mobNumber = "PIN: 923009"+formatedDate();
+		System.out.println(mobNumber);
+		//txtEmail.sendKeys("vickstester@yahoo.com");
+		txtPassword.sendKeys("123456");
+		txtConfirmPassword.sendKeys("123456");
+		clickUsingJS(chkTermsAndPrivacy);
+		hardwait(5);
+	}
+	
+	public void ClickSignup(){
+		btnSignup.click();
+		hardwait(15);
+	}
+	
+	public void ClickUserName(){
+		linkUserName.click();
+		hardwait(1);
+	}
+	
+	public void ClickMyAccount(){
+		linkMyAccount.click();
+		hardwait(5);
+	}
+	
+	public void GetPinNumberAndVerify(){
+		String customerPinNumber = pinNumber.getText();
+		assertEquals(customerPinNumber, mobNumber);
+		System.out.println("Customer Pin Number is"+customerPinNumber);
+		System.out.println("Provided Mobile number is"+mobNumber);
+	}
+		
+	
 }
